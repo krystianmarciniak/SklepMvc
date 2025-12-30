b4c105f1912a315c6b2cf518ab6457e6c218b680
Krystian Marciniak <krystianmarciniak446@gmail.com>
2025-12-30 02:51:13 +0100

    Add Products CRUD (Admin only)

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,7 +7,7 @@ public class Product
   public int Id { get; set; }
 
   [Required, StringLength(100)]
-  public string Name { get; set; } = string.Empty;
+  public string Name { get; set; } = "";
 
   [StringLength(500)]
   public string? Description { get; set; }
@@ -16,7 +16,7 @@ public class Product
   public decimal Price { get; set; }
 
   [Range(0, 100000)]
-  public int Stock { get; set; }
+  public int Stock { get; set; } = 0;
 
   public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
 }
